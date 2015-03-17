f9dafafff777f6a6406132647effe8e6c5c7d59f
Gunlek <fabien.aubret@gmail.com>
2015-03-17 23:12:55 +0100

    Create main.php

---
+++ Basic_button/main.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Button_animation - Simple-Duino</title>
+		<meta charset="utf-8" />
+		<link rel="stylesheet" href="button_animation.css" />
+		<script type="text/javascript" src="jquery.js"></script>
+		<script type="text/javascript" src="button_animation.js"></script>
+	</head>
+	<body>
+
+		<button id="animated_button" class="example_button">Hello world !</button>
+
+	</body>
+</body>
